b984a66d0341d1f5df7d56a2c01ba1d5a6b39e52
ravi819 <77319151+ravi819@users.noreply.github.com>
2026-03-17 20:59:59 +0530

    Create Student Controller

---
+++ controllers/studentController.js
@@ -0,0 +1,9 @@
+// Placeholder controller for Student module
+
+exports.getAllStudents = (req, res) => {
+    res.send('Get all students');
+};
+
+exports.createStudent = (req, res) => {
+    res.send('Create new student');
+};
