487818dda505d48c259afac67a7f80f52543d139
ravi819 <77319151+ravi819@users.noreply.github.com>
2026-03-17 21:03:08 +0530

    Create Student Management component

---
+++ src/components/StudentManagement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentManagement = () => {
+    return (
+        <div>
+            <h1>Student Management</h1>
+        </div>
+    );
+};
+
+export default StudentManagement;
